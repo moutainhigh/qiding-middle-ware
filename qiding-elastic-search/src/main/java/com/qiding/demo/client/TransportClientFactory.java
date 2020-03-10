77a90b6dccfb41a8ed2bd946509d7f3f87b44a1d
qiding <qiding@syswin.com>
2020-03-10 17:53:09 +0800

    项目初始化

--- qiding-elastic-search/src/main/java/com/qiding/demo/client/TransportClientFactory.java
+++ qiding-elastic-search/src/main/java/com/qiding/demo/client/TransportClientFactory.java
@@ -6,6 +6,7 @@ import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -22,12 +23,16 @@ public class TransportClientFactory {
 	public TransportClient client(String clusterName) throws UnknownHostException {
 		Settings settings = Settings.builder()
 			.put("cluster.name", "qiding-cluster")
+//			.put("cluster.name", "hello-world")
 			.put("client.transport.sniff",true)
 			.build();
 		TransportClient client = new PreBuiltTransportClient(settings)
-			.addTransportAddress(new TransportAddress(InetAddress.getByName("master"), 9300))
-			.addTransportAddress(new TransportAddress(InetAddress.getByName("node2"), 9300))
-			.addTransportAddress(new TransportAddress(InetAddress.getByName("node1"), 9300));
+			.addTransportAddress(new TransportAddress(InetAddress.getByName("master"), 9300));
+//			.addTransportAddress(new TransportAddress(InetAddress.getByName("node2"), 9300))
+//			.addTransportAddress(new TransportAddress(InetAddress.getByName("node1"),9300));
+//			.addTransportAddress(new TransportAddress(InetAddress.getLocalHost(),9300));
+
+
 		clientMap.put(clusterName,client);
 		return client;
 	}
---
+++ qiding-h2/src/main/java/com/syswin/h2/QidingH2Application.java
@@ -0,0 +1,13 @@
+package com.syswin.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QidingH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QidingH2Application.class, args);
+	}
+
+}
