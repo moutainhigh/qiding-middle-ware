3da086a72f371beb9545dafb8d4a5d4bd2c89f11
齐定 <qiding@syswin.com>
2020-04-13 16:26:23 +0800

    aaa

--- qiding-drools/src/main/java/com/qiding/drools/App.java
+++ qiding-drools/src/main/java/com/qiding/drools/App.java
@@ -1,14 +1,23 @@
 package com.qiding.drools;
 
 import org.kie.api.KieServices;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.command.Command;
+import org.kie.api.runtime.ExecutionResults;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
 import org.kie.api.runtime.rule.FactHandle;
+import org.kie.internal.command.CommandFactory;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Hello world!
@@ -22,7 +31,35 @@ public class App
         KieServices kieServices = KieServices.Factory.get();
 		KieContainer kieContainer =kieServices.getKieClasspathContainer();
 		KieSession kieSession = kieContainer.newKieSession("all-rules");
-	   final HelloWorld helloWorld=	HelloWorld.builder().age(100).name("qi").gone(false).build();
+
+
+
+		//基本对象
+		final HelloWorld qi=	HelloWorld.builder().age(100).name("qi").gone(false).build();
+		final HelloWorld ding=	HelloWorld.builder().age(100).name("ding").gone(false).build();
+		StatelessKieSession statelessKieSession = kieContainer.newStatelessKieSession("all-rules-2");
+		statelessKieSession.execute(CommandFactory.newInsert(qi));
+		qi.setGone(true);
+		//数组对象
+		statelessKieSession.execute(Arrays.asList(qi,ding));
+		//批量处理
+		List<Command> commands= Stream.of(
+			CommandFactory.newInsert(HelloWorld.builder().age(100).name("qi").gone(false).build(),"qi"),
+			CommandFactory.newInsert(HelloWorld.builder().age(100).name("qi").gone(false).build(),"ding")
+		).collect(Collectors.toList());
+		ExecutionResults results=statelessKieSession.execute(CommandFactory.newBatchExecution(commands));
+		System.out.println(results.getValue("qi"));
+		System.out.println(results.getValue("ding"));
+
+
+
+
+
+
+
+
+
+		final HelloWorld helloWorld=	HelloWorld.builder().age(100).name("qi").gone(false).build();
 		BlockingQueue<HelloWorld> blockingQueue=new ArrayBlockingQueue<HelloWorld>(3);
 
 		new Thread(()->{
@@ -33,7 +70,11 @@ public class App
 //				e.printStackTrace();
 //			}
 			kieSession.insert(helloWorld);
+			FactHandle handle=kieSession.getFactHandle(helloWorld);
 			kieSession.fireAllRules();
+			helloWorld.setGone(true);
+			FactHandle handle2=kieSession.getFactHandle(helloWorld);
+			System.out.println("11111");
 		}).start();
 
 		TimeUnit.SECONDS.sleep(5);
--- qiding-poi/src/main/java/com/qiding/poi/syswin/UserAttendanceHandler.java
+++ qiding-poi/src/main/java/com/qiding/poi/syswin/UserAttendanceHandler.java
@@ -1,4 +1,52 @@
 package com.qiding.poi.syswin;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserAttendanceHandler {
+
+	public void readUserAttendanceData(String fileName, Integer sheetIndex, Integer begin, Integer end) throws IOException {
+		InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+
+		List<UserAttendance> infoList = new ArrayList<>();
+
+		XSSFSheet sheet = workbook.getSheetAt(sheetIndex);
+		for (int i = begin; i < end; i++) {
+			XSSFRow row = sheet.getRow(i);
+			String name = row.getCell(0).getStringCellValue();
+			String userNo =   Double.valueOf(row.getCell(1).getNumericCellValue()).intValue()+"";
+			String userAttendNo =  row.getCell(2).getStringCellValue();
+			String userId = row.getCell(3).getStringCellValue();
+			infoList.add(UserAttendance.builder()
+				.userId(userId)
+				.userName(name)
+				.userNo(userNo)
+				.attendanceNo(userAttendNo)
+				.timeTimeStamp(Instant.now().toEpochMilli())
+				.build());
+		}
+
+		StringBuffer stringBuffer = new StringBuffer();
+
+		infoList.stream().forEach(info -> stringBuffer.append(info.toString()).append(","));
+		System.out.println(stringBuffer);
+		inputStream.close();
+		workbook.close();
+	}
+
+
+	public static void main(String[] args) throws IOException {
+		UserAttendanceHandler userAttendanceHandler=new UserAttendanceHandler();
+		userAttendanceHandler.readUserAttendanceData("地产秘邮打卡问题明细汇总.xlsx",2,1,11);
+
+	}
+
 }
